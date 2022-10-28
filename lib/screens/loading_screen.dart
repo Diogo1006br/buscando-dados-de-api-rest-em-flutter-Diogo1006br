7f1c98e55aa92b4a3046df2446ad258baf9d5ee4
Antoino <89143867+Diogo1006br@users.noreply.github.com>
2022-10-28 17:40:56 -0300

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tempo_template/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:tempo_template/services/weather.dart';
+
+const apiKey = '566d412a9e457bb3e4017109ed134985';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,17 +13,34 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude = 0;
+  late double longitude = 0;
+
+  void pushToLocationScreen(dynamic weatherData) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(localWeatherData: weatherData);
+    }));
+  }
+
+  void getData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+    this.pushToLocationScreen(weatherData);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // obtém a localização atual
-          },
-          child: const Text('Obter Localização'),
-        ),
+    return const Center(
+      child: SpinKitFadingFour(
+        color: Colors.white,
+        size: 100.0,
       ),
     );
   }
 }
+
